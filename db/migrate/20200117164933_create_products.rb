29501e520235d6eecdfc13572832d9d89bd86b47
Marcus Jensen <marcusjensen1515@gmail.com>
2020-01-17 09:20:17 -0800

    create databases

--- db/migrate/20200117164933_create_products.rb
+++ db/migrate/20200117164933_create_products.rb
@@ -1,6 +1,10 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
+      t.column(:name , :string)
+      t.column(:cost , :float)
+      t.column(:country_of_origin , :string)
+
     end
   end
 end
