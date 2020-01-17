c5b1fa048796029dfc67e3b9470e1db133ebb47f
Marcus Jensen <marcusjensen1515@gmail.com>
2020-01-17 11:43:25 -0800

    app now captializes product names and country of origin

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,13 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  #this before save is new
+  before_save :capitalize
+
+  #this capitalize method is new
+  def capitalize
+    self.name = self.name.titleize()
+    self.country_of_origin = self.country_of_origin.titleize()
+  end
+
+  #dont get rid of this end below
 end
