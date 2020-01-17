fb55eaf8137064a6c36e1b230ad1144569b48e93
Marcus Jensen <marcusjensen1515@gmail.com>
2020-01-17 11:10:50 -0800

    full CRUD functionality now available for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,26 +6,57 @@ class ReviewsController < ApplicationController
 
   def new
     # Code for new album form goes here.
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new
+    render :new
   end
 
   def create
     # Code for creating a new album goes here.
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+      if @review.save
+        redirect_to product_path(@product)
+      else
+        render :new
+      end
   end
 
   def edit
     # Code for edit album form goes here.
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    render :edit
   end
 
   def show
     # Code for showing a single album goes here.
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    render :show
   end
 
   def update
     # Code for updating an album goes here.
+    @review = Review.find(params[:id])
+      if @review.update(review_params)
+        redirect_to product_review_path(@review.product)
+      else
+        render :edit
+      end
   end
 
   def destroy
     # Code for deleting an album goes here.
+
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to product_path(@review.product)
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:author, :content_body, :rating, :product_id)
   end
 
 end
