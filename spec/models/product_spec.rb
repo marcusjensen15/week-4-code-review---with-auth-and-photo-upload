c1c6b8d49987e4a0603859614178b07c1e1bc9f1
Marcus Jensen <marcusjensen1515@gmail.com>
2020-01-17 09:37:57 -0800

    create appropriate routes for product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 describe Product do
-  # Tests will go here.
+  it { should have_many(:reviews) }
+
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 describe Review do
-  # Tests will go here.
+  it { should belong_to(:product) }
+
 end
